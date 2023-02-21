1564f5a25521b641f38acd7dbf9ac476cffa9107
20180429billy <82895929+20180429billy@users.noreply.github.com>
2023-02-21 05:45:11 -0600

    Add files via upload

---
+++ TECNOMASTER/privado/js/app.js
@@ -0,0 +1,11 @@
+const btnAbrirModal = document.querySelector("#btn-abrir-modal");
+const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+const modal = document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click", (event)=>{ 
+    modal.showModal();
+});
+
+btnCerrarModal.addEventListener("click", ()=>{
+    modal.close();
+});
